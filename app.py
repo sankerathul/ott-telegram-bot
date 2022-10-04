853f0448f6c392cf45b7201f873f43f910e4a061
athul3 <athul@wandx.co>
2022-10-05 01:17:29 +0530

    logging added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import dotenv
 import telebot
+import logging
 
 dotenv.load_dotenv()
 
@@ -8,9 +9,13 @@ BOT_API_KEY = os.getenv('BOT_API_KEY')
 
 bot = telebot.TeleBot(BOT_API_KEY)
 
+logging.basicConfig(filename="app.log", level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', filemode="w")
+
+logging.info(f'[INFO] BOT Server Started')
 
 @bot.message_handler()
 def start(message):
+    logging.info(f'[INFO] Message Received --> {message.chat.id} : {message.text}')
     bot.send_message(message.chat.id, "OTT Hunter Coming Soon\nStay Tuned")
 
 bot.polling()
